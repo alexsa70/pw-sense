f4e8cf02d72fb3b1e2eb04df7b80545996d81542
alexsa70 <40318204+alexsa70@users.noreply.github.com>
2026-01-29 07:49:22 +0200

    playwright.config.ts - new settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,7 @@ import { defineConfig, devices } from '@playwright/test';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
+ * Playwright Configuration for KalSense proect
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -20,16 +21,35 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /*Reporters*/
+  reporter: [
+    ['html'],
+    ['list'],
+    ['json', { outputFile: 'test-results/results.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Maximum time per test */
+  timeout: 30 * 1000,
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://kal-sense.prod.kaleidoo-dev.com',
+
+    /* Screenshot only when falling */
+    screenshot: 'only-on-failure',
+
+    /* Video only when falling */
+    video: 'retain-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Тimeout for navigation */
+    navigationTimeout: 15 * 1000,
+
+    /*Timeout for actions */
+    actionTimeout: 10 * 1000,
   },
 
   /* Configure projects for major browsers */
@@ -44,10 +64,11 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // Uncomment if necessary webkit
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -76,4 +97,6 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  /* Folder for screenshots, videos, and traces */
+  outputDir: 'test-results/',
 });
